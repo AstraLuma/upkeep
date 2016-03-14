fa34221602abd2c436d78d08ffa7214c5146125c
Jamie Bliss <astronouth7303@gmail.com>
2016-03-13 21:02:06 -0400

    A bunch of front-end work

--- static/things/jobs.js
+++ static/things/jobs.js
@@ -1,16 +1,30 @@
 $$.q('[data-action=finish-job]').forEach(function(ele) {
     ele.addEventListener("click", function(event) {
-        var jobid = event.target.dataset.jobid;
+        var jobid = ele.dataset.jobid;
         $$.json.post("/finish.json", {'job': jobid})
         .then(function() {
-            event.target.innerText = "Ok!";
-            event.target.disabled = true;
+            ele.innerText = "Ok!";
+            ele.disabled = true;
         });
     });
 });
 
 $$.q('[data-action=show-dialog]').forEach(function(ele) {
     ele.addEventListener("click", function(event) {
-        $$.id(event.target.dataset.dialog).showModal();
+        $$.id(ele.dataset.dialog).showModal();
+    });
+});
+
+$$.q('[data-action=close]').forEach(function(ele) {
+    ele.addEventListener("click", function(event) {
+        ele.parentNode.close();
+    });
+});
+
+$$.q('[data-action=delete-schedule]').forEach(function(ele) {
+    ele.addEventListener("click", function(event) {
+        if (window.confirm("Are you sure you want to delete "+ele.dataset.label+"?")) {
+            console.log("Delete schedule", ele.dataset.schedule);
+        }
     });
 });
