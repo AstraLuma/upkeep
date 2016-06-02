e2e10b492e0585266ede0cf95dc0ab80d57d86c7
Jamie Bliss <astronouth7303@gmail.com>
2016-06-02 12:53:23 -0400

    Handle the anonymous user case in get_things_for()

--- things/templatetags/things.py
+++ things/templatetags/things.py
@@ -5,4 +5,5 @@ register = template.Library()
 
 @register.simple_tag
 def get_things_for(user):
-    return Thing.objects.filter(user=user)
+	if user.is_authenticated():
+		return Thing.objects.filter(user=user)
