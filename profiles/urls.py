bf5ae9b78a7476aca9e5c8f9bf756312fb7594e3
Jamie Bliss <astronouth7303@gmail.com>
2016-06-01 21:49:51 -0400

    Give useful information on the index

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import url, include
-from django.views.generic.base import TemplateView
-from .views import IndexView
+from .views import index
 
 urlpatterns = [
-    url('^$', IndexView.as_view(), name='index'),
+    url('^$', index, name='index'),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,21 @@
+from django import forms
 from django.shortcuts import render
-from django.views.generic.base import TemplateView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
+from piston import get_registrations, url2email
 
-class IndexView(LoginRequiredMixin, TemplateView):
-	template_name='profiles/index.html'
+class EmailForm(forms.Form):
+    email = forms.EmailField(label="Email")
+
+@login_required
+def index(request):
+    browsers = 0
+    emails = set()
+    for url, typ in get_registrations(request.user):
+        if typ == 'web':
+            browsers += 1
+        elif typ == 'email':
+            emails.add(url)
+    return render(request, "profiles/index.html", {
+        'browsers': browsers,
+        'emails': [(e, url2email(e)) for e in sorted(emails)],
+        })
